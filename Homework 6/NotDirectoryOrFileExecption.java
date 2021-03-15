6fdb08e62b5c0b2fe2516a0100dccf7e704f81fc
Alexander11001 <75197722+Alexander11001@users.noreply.github.com>
2021-03-15 21:10:46 +0300

    Add files via upload

---
+++ Homework 6/NotDirectoryOrFileExecption.java
@@ -0,0 +1,11 @@
+package HomeworkCollections;
+
+public class NotDirectoryOrFileExecption extends Exception {
+
+
+    public NotDirectoryOrFileExecption(String message) {
+
+        super(message);
+
+    }
+}
